5cc0a2321886048eea03de9bb2176da12706af93
PhungNguyen11 <126753065+PhungNguyen11@users.noreply.github.com>
2023-03-02 19:44:16 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,15 +3,15 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: 'niviki-com-t-do-t-i-ch-nh-10f5e54221eb4b059bc5b5305da949ba',
-  rootNotionPageId: 'Simple-Template-49955b133eb843bdb8665534ecfd844c',
+  rootNotionPageId: 'NIVIKI-COM-TEMPLATE-88ee8e2d4a7743aca52a1b475d49d0ae',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'NIVIKI.COM - TỰ DO TÀI CHÍNH',
+  name: 'MeMo',
   domain: 'niviki.com',
-  author: 'Khoa Nguyen',
+  author: 'Phung_Nguyen',
 
   // open graph metadata (optional)
   description: 'Lan toả tinh thần tự do tài chính đến với nhiều người hơn nữa! Tự do tài chính là bạn được quyền “thất nghiệp” nhưng vẫn có đủ tiền cho một cuộc sống thoải mái trong thời gian dài',
@@ -21,7 +21,7 @@ export default siteConfig({
   // github: 'transitive-bullshit',
   linkedin: 'khoanguyen08',
   
-  facebook: 'https://www.facebook.com/groups/nocodevietnam',
+  facebook: 'https://www.facebook.com/vanphung.nguyen.31',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
